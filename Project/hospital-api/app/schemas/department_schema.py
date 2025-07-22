7d82683e6cfcb07762625f11d2c7fa19f94e56bf
Pankti Nareshkumar Sharma <panktis968@gmail.com>
2025-07-22 21:15:40 +0000

    unnecessary files deleted and requiremnts.txt file update

--- Project/hospital-api/app/schemas/department_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class DepartmentCreate(BaseModel):
-    name: str
-    description: str
-
-class DepartmentOut(DepartmentCreate):
-    id: int
--- Project/hospital-api/init_db.py
+++
@@ -1,6 +0,0 @@
-from app.database import Base, engine
-from app import models  # This ensures tables are loaded
-
-print("Creating tables in test.db...")
-Base.metadata.create_all(bind=engine)
-print("Done.")
